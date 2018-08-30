8066cf2acfedabf698026a6cc411817f22edc9e2
Jack Hyc <Jacek.Hyc@gmail.com>
2018-08-30 18:23:04 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PracticeOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myCars = new string[4] { "Mazda Miata", "Ford Model T", "Dodge Challenger", "Nissan 300zx"};
+// The 'Length' property tells us how many values are in the Array (4 in our example here). 
+// We can use this to determine where the loop ends: Length-1 is the index of the last value. 
+            for (int idx = 0; idx < myCars.Length; idx++)
+            {
+                Console.WriteLine("I own a {0}", myCars[idx]);
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
